80f4d3eb7dee384d3633fbb73ec6d126fc084a3d
SiddharthaG09 <78454757+SiddharthaG09@users.noreply.github.com>
2022-10-17 21:15:49 -0500

    Create RaterTesting.java

---
+++ 3.3.7/RaterTesting.java
@@ -0,0 +1,9 @@
+public class RaterTesting
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Rater first = new Rater("Sid's Bakery" , 5.0);
+        Rater second = new Rater("Sid's Cafe" , 2.5);
+    }
+}
