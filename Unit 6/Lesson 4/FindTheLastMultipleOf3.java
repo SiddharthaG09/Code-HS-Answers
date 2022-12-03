ea2dadc980ebeca859c1fc695ea79942e8acbb40
Siddhartha Guntupalli <siddu@guntupalli.net>
2022-12-02 23:08:08 -0600

    added lesson 4

---
+++ Unit 6/Lesson 4/FindTheLastMultipleOf3.java
@@ -0,0 +1,21 @@
+public class LastMultipleOfThree {
+
+    public static void main(String[] args) {
+        int[] numbers1 = { 76, 75, 3, 17, 12, 22, 7 };
+        System.out.print("The last multiple of 3 is " + findMultipleOfThree(numbers1));
+
+    }
+
+    public static int findMultipleOfThree(int[] arr) {
+        // your code goes here!
+        int multiple = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 3 == 0) {
+                multiple = arr[i];
+            }
+        }
+
+        return multiple;
+    }
+}
